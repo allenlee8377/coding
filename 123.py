0590c60d1d567f2cb87c26856989a8554325bd78
allenlee <allenlee@synology.com>
2023-07-20 16:45:42 +0800

    add test12

---
+++ 123.py
@@ -0,0 +1,12 @@
+#練習: 輸入攝氏溫度，轉成華氏溫度
+
+#temperture: tmp
+tmp = input('plz enter C-degree temperture, I will transfer it:  ')
+tmp = int(tmp)
+
+tmp2 = tmp*9/5+32
+print('the result is :',tmp2)
+
+
+#F=C*9/5+32
+#C=(F-32)*5/9
