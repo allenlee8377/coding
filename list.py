a3e12ea96e7492886821bf5e2a5e9f4e1dbcb3c6
allenlee <allenlee@synology.com>
2023-08-01 14:46:41 +0800

    Signed-off-by: allenlee <allenlee@synology.com>

---
+++ list.py
@@ -0,0 +1,11 @@
+#清單 = list = 箱子
+
+a = [123,546,True,'AA','BB']
+print(a)
+print(a[0])
+print(a[2])
+a.append('888')
+print(a)
+print(len(a)) #len = length
+print('123' in a) #in 的後面會接個清單
+print(123 in a)
